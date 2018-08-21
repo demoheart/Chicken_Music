1e33397203864fc6248c81c2cb2d9cfe083f0548
wumeng <2290975269@qq.com>
2018-08-21 18:44:56 +0800

    创建vue音乐项目，实现轮播图

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+
+import 'common/stylus/index.styl'
+
+// 减少手机点击 300 毫秒的延时
+fastclick.attach(document.body)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: c => c(App)
+})
