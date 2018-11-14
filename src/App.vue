91f811adc8ea439850107e1ca8993f67e6cddf47
wumeng <2290975269@qq.com>
2018-11-14 21:44:48 +0800

    加了个空格

--- src/App.vue
+++ src/App.vue
@@ -9,19 +9,19 @@
   </div>
 </template>
 
+
 <script type="text/ecmascript-6">
-import MHeader from 'components/m-header/m-header'
-import Tab from 'components/tab/tab'
-import Player from 'components/player/player'
+import MHeader from "components/m-header/m-header";
+import Tab from "components/tab/tab";
+import Player from "components/player/player";
 export default {
   components: {
     MHeader,
     Tab,
     Player
   }
-}
+};
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-
 </style>
